2bdd392ad02f0121f9185b1525aa00f401336884
Limpos1 <rkddnwlals1@naver.com>
2024-08-15 13:46:23 +0900

    초기 ì 세팅

---
+++ src/main/java/com/sparta/schedulemanagement/Controller/ScheduleController.java
@@ -0,0 +1,8 @@
+package com.sparta.schedulemanagement.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ScheduleController {
+
+}
---
+++ src/main/java/entity/Schedule.java
@@ -0,0 +1,11 @@
+package entity;
+
+public class Schedule {
+    private int id;
+    private String password;
+    private String managername;
+    private String date;
+    private String fix_date;
+
+
+}
